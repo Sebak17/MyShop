8eb848db31239aec43b2134643fcebe52b5e83b3
Sebastian <sebakrysa@wp.pl>
2019-10-25 13:44:38 +0200

    Create ProductImage.php

---
+++ app/ProductImage.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductImage extends Model
+{
+    
+	protected $table = 'products_image';
+
+	public $timestamps = false;
+
+	protected $fillable = [
+        'product_id', 'name'
+    ];
+
+	public function product()
+    {
+        return $this->belongsTo('App\Product');
+    }
+
+}
