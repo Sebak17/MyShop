d211b6ebc27d995c87dc8b2268cf898d117860ba
Sebak17 <sebakrysa@wp.pl>
2019-10-27 22:28:29 +0100

    Added 2 filters and small fix

--- public/js/_admin.products.list.js
+++ public/js/_admin.products.list.js
@@ -8,12 +8,12 @@ window.onload = function () {
 }
 
 function screenProductsEmpty() {
-    $('#productsList').html(String.raw`<tr class="table-danger"><td class="text-center"><i class="fas fa-times-circle"></i> Nie znaleziono produktów!</td></tr>`);
+    $('#productsList').html(String.raw`<tr class="table-danger"><td class="text-center" colspan="4"><i class="fas fa-times-circle"></i> Nie znaleziono produktów!</td></tr>`);
     $('#productsAmount').html("0");
 }
 
 function screenLoading() {
-    $('#productsList').html(String.raw`<tr><td class="text-center"><i class="fas fa-circle-notch fa-spin fa-3x"></i> <h4>Ładowanie produktów...</h4></td></tr>`);
+    $('#productsList').html(String.raw`<tr><td class="text-center" colspan="4"><i class="fas fa-circle-notch fa-spin fa-3x"></i> <h4>Ładowanie produktów...</h4></td></tr>`);
     $('#productsAmount').html("?");
 }
 
@@ -31,6 +31,28 @@ function loadProducts() {
 
         params['name'] = sr_name;
     }
+
+    let sr_minPrice = $("#inp_price1").val();
+    if(sr_minPrice != '') {
+        if(!validateProductPrice(sr_minPrice)) {
+            showAlert(AlertType.ERROR, Lang.PRODUCT_PRICE_ERROR);
+            return;
+        }
+
+        params['minPrice'] = sr_minPrice;
+    }
+
+    let sr_maxPrice = $("#inp_price2").val();
+    if(sr_maxPrice != '') {
+        if(!validateProductPrice(sr_maxPrice)) {
+            showAlert(AlertType.ERROR, Lang.PRODUCT_PRICE_ERROR);
+            return;
+        }
+
+        params['maxPrice'] = sr_maxPrice;
+    }
+
+
 
     $.ajax({
         url: "/systemAdmin/productLoadList",
@@ -55,8 +77,12 @@ function loadProducts() {
                                             <td class="align-middle text-left">
                                                 <h5>` + data.list[i].name + `</h5>
                                             </td>
+                                            <td class="align-middle text-left">
+                                                <h5>` + data.list[i].price + ` zł</h5>
+                                            </td>
                                             <td class="align-middle text-right">
-                                                <button class="btn btn-primary"><i class="fas fa-edit"></i> Edytuj</button>
+                                                <button class="btn btn-info"><i class="fas fa-arrow-right"></i> Pokaż</button>
+                                                <a href="/admin/produkty/edytuj/` + data.list[i].id  + `"><button class="btn btn-primary"><i class="fas fa-edit"></i> Edytuj</button></a>
                                             </td>
                                         </tr>`;
 
