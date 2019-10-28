d807ee1be9fa95747359a84ee96065a1efcd3ad3
Sebak17 <sebakrysa@wp.pl>
2019-10-28 14:11:54 +0100

    Small fix in admin panel view

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -1,8 +1,8 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 	
-	<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-		<div class="sidebar-brand-icon rotate-n-15">
-			<i class="fas fa-laugh-wink"></i>
+	<a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('admin_dashboardPage') }}">
+		<div class="sidebar-brand-icon">
+			<i class="fas fa-tachometer-alt"></i>
 		</div>
 		<div class="sidebar-brand-text mx-3">Admin Panel</div>
 	</a>
@@ -11,7 +11,7 @@
 	
 	<li class="nav-item">
 		<a class="nav-link" href="{{ route('admin_dashboardPage') }}">
-			<i class="fas fa-fw fa-tachometer-alt"></i>
+			<i class="fas fa-fw fa-columns"></i>
 			<span>Panel główny</span></a>
 		</li>
 		
