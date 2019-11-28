0ff2a2e831c07b6e232a35633f865f34e925cfae
Sebastian <sebakrysa@wp.pl>
2019-11-28 12:02:03 +0100

    Create PayPalController.php

---
+++ app/Http/Controllers/Payments/PayPalController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Payments;
+
+use Illuminate\Http\Request;
+
+class PayPalController extends Controller
+{
+    
+	public function __construct()
+	{
+		
+
+		
+	}
+
+}
