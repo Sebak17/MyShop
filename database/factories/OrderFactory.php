c1c8ed5e9d64b97c55decc71984eeecaafa9e0be
Sebak17 <sebakrysa@wp.pl>
2020-03-05 19:48:58 +0100

    🛠 Fixed order factory

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -10,9 +10,9 @@ $factory->define(Order::class, function (Faker $faker) {
         'status'       => 'CREATED',
         'cost'         => $faker->randomFloat(2, 1, 10000),
         'buyer_info'   => json_encode([
-            'clientFName' => $faker->firstName,
-            'clientSName' => $faker->lastName,
-            'clientPhone' => $faker->numberBetween(111111111, 999999999),
+            'firstname' => $faker->firstName,
+            'surname' => $faker->lastName,
+            'phone' => $faker->numberBetween(111111111, 999999999),
         ]),
         'deliver_name' => "COURIER",
         'deliver_info' => json_encode([
